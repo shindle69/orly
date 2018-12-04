8e4667257a9cbc04b3b0ef907b8e1c29735eb747
APPLE <apple@APPLEui-iMac.local>
2018-12-04 13:58:35 +0900

    cover / layout.html 생성

--- orly/urls.py
+++ orly/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path , include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cover/', include('cover.urls')),
+    path('',lambda r:redirect('cover:index')),
 ]
