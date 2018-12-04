ddced9dcc00cb671e77e07ddc95c6e5e5c1539ca
APPLE <apple@APPLEui-iMac.local>
2018-12-04 17:42:33 +0900

    form 받아서 그림에 그려주는 것

--- cover/views.py
+++ cover/views.py
@@ -25,12 +25,16 @@ def image_generator(request):
     im=Image.new('RGB',(256,256),'white')
 
     ttf_path=settings.ROOT('assets','fonts','NanumBarunGothicBold.ttf')
-    # get a font
-    fnt = ImageFont.truetype(ttf_path, 40)
-    # get a drawing context
+    # get a font    
     d = ImageDraw.Draw(im)    
-    d.text((10,10), title , font=fnt, fill=(0,255,0,128))    
+
+    fnt = ImageFont.truetype(ttf_path, 40)
+    d.text((10,10), title , font=fnt, fill=(0,255,0,128))  
+
+    fnt = ImageFont.truetype(ttf_path, 30)
     d.text((10,60), top_text , font=fnt, fill=(0,255,0,255))
+
+    fnt = ImageFont.truetype(ttf_path, 20)
     d.text((10,110), author , font=fnt, fill=(0,255,0,255))
 
     response=HttpResponse(content_type='image/png')
