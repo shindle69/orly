cbed2562f3aca3e39ba6d3cfcc1eafda65c73563
APPLE <apple@APPLEui-iMac.local>
2018-12-04 18:32:16 +0900

    컬러코드 및 사진 이미지 다운로드세팅

--- cover/forms.py
+++ cover/forms.py
@@ -5,6 +5,17 @@ class CoverForm(forms.Form):
     title = forms.CharField()
     top_text = forms.CharField()
     author = forms.CharField()
+    animal_code = forms.ChoiceField(choices=[(i,i) for i in range(1,41)])
+    color_code = forms.ChoiceField(choices=[(i,i) for i in range(1,17)])
+    guide_text = forms.CharField()
+    guide_text_placement = forms.ChoiceField(
+        choices=[
+            ('bottom_right','bottom_right'),
+            ('bottom_left','bottom_left'),
+            ('top_right','top_right'),
+            ('top_left','top_left'),
+        ]
+    )
 
     @property
     def get_params(self):
