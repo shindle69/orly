f82861c7ff4469b145b95aa45506e48e65cc4862
APPLE <apple@APPLEui-iMac.local>
2018-12-06 16:08:38 +0900

    bootstrap 적용

--- orly/settings/common.py
+++ orly/settings/common.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'bootstrap4',
+
     'cover',
 ]
 
@@ -122,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILE_DIRS=[
+    ROOT('orly','static'),
+]
